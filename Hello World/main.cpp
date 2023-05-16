96d76b8d199e037d1d1a97f14bcaaf443628d5ea
Rhys2000 <rhys2kjj@gmail.com>
2023-05-15 20:42:25 -0700

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello World
+//
+//  Created by Rhys Julian-Jones on 5/15/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
