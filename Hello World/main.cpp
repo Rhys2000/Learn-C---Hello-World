08938264fa49e1b36ca2992080d740bcbc684247
Rhys2000 <rhys2kjj@gmail.com>
2023-05-15 20:58:42 -0700

    Completed the first three lessons of this chapter

--- Hello World/main.cpp
+++ Hello World/main.cpp
@@ -1,14 +1,38 @@
-//
 //  main.cpp
 //  Hello World
-//
 //  Created by Rhys Julian-Jones on 5/15/23.
-//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main() {
+    /*
+    - C++ is a programming language created by Bjarne Stroustrup and his team at Bell Laboratories in 1979. Forty years later, it is one of the most widely used languages in the world; we can find C++ applications everywhere, from the bottom of the oceans to the surface of Mars.
+    - As the name implies, C++ was derived from the C language; Bjarne’s goal was to add object-oriented programming into C, a language well-respected for its portability and low-level functionality.
+    - So why learn C++? Among many other things:
+        > It is fast and flexible.
+        > It is well-supported.
+        > It forces you to think in new and creative ways.
+    - In this lesson, we’ll start learning some basic concepts, and you’ll write your very first C++ program.
+     
+    - C++ programs are stored in files which usually have the file extension .cpp, which simply stands for “C Plus Plus”.
+    - The code inside our C++ file is a classic first step all new programmers take — they greet the world through the terminal!
+    - The terminal is the black panel on the bottom. It should be blank right now. The code in the text editor will print text out onto the terminal. More specifically, it will print the phrase Hello World!.
+    */
+    
+    std::cout << "Hello World!\n";
+    
+    /*
+    - High five! We just got your first program to run.
+    - C++, like most programming languages, runs line by line, from top to bottom.
+    - In between the curly braces is what we are going to focus on for now.
+        std::cout << "Hello World!\n";
+    - std::cout is the “character output stream”. It is pronounced “see-out”.
+    - << is an operator that comes right after it.
+    - "Hello World!\n" is what’s being outputted here. You need double quotes around text. The \n is a special character that indicates a new line.
+    - ; is a punctuation that tells the computer that you are at the end of a statement. It is similar to a period in a sentence.
+    */
+    
+    std::cout << "Codecademy\n";
+    
+    
 }
